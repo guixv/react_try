3eb5f05af87549c773cac3d65d17d655a9721d9a
guixv <954182519@qq.com>
2019-08-19 23:33:46 +0800

    Updates

---
+++ precache-manifest.162e38b961dbad128aef2ef86588a9d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7ddc7f1e917d482675602328c9d4d3",
+    "url": "/react_try.git/index.html"
+  },
+  {
+    "revision": "35d7eb47ceab3d394c92",
+    "url": "/react_try.git/static/css/main.51d1f949.chunk.css"
+  },
+  {
+    "revision": "2e32d29f5be559d32a16",
+    "url": "/react_try.git/static/js/2.e08c57b3.chunk.js"
+  },
+  {
+    "revision": "35d7eb47ceab3d394c92",
+    "url": "/react_try.git/static/js/main.c260488a.chunk.js"
+  },
+  {
+    "revision": "a180adf535262ce6fcf5",
+    "url": "/react_try.git/static/js/runtime~main.29ff5c20.js"
+  }
+]);
